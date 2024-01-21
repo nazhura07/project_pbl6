715a75d4c19c3de30c830d551754f5c3149e00e8
NasFahmi <75nasrulfahmi@gmail.com>
2024-01-21 20:27:59 +0700

    fix klien jenis kelamin

--- database/migrations/2024_01_20_055709_create_kliens_table.php
+++ database/migrations/2024_01_20_055709_create_kliens_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('nama');
             $table->string('password');
-            $table->string('jenis_kelamin');
+            $table->boolean('jenis_kelamin');
             $table->string('alamat');
             $table->string('email');
             $table->timestamps();
